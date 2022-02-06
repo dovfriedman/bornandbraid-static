6c5e51e58878ab246a0e4a31b82874bb4ccfbc5d
dovfriedman <dov@dovfriedman.com>
2022-02-06 22:39:05 +0000

    Deploying to main from @ dovfriedman/bornandbraid-source@bd8ecdc0432572b45b818770e870b179faa6f706 🚀

---
+++ _next/static/Ai-lUdcP0T1igwiGNHpO5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f0e2c9b0e09679ba.css","static/chunks/pages/index-7c027f7f855b6ada.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
